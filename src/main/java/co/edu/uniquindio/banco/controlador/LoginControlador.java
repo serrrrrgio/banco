168c2b6510536953df5ca080790efc6edb134dba
serrrrrgio <sergio.posadag@uqvirtual.edu.co>
2025-04-05 17:24:40 -0500

    se crea el LoginControlador y se agrega el metodo iniciarSesion tanto a banco como al controller

--- src/main/java/co/edu/uniquindio/banco/controlador/LoginControlador.java
+++ src/main/java/co/edu/uniquindio/banco/controlador/LoginControlador.java
@@ -1,10 +1,61 @@
 package co.edu.uniquindio.banco.controlador;
 
+import co.edu.uniquindio.banco.modelo.entidades.Banco;
+import co.edu.uniquindio.banco.modelo.entidades.Usuario;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
 /**
  * Clase que representa el controlador de la vista de login
  * @author caflorezvi
  */
 public class LoginControlador {
+
+    private final Banco banco;
+
+    public LoginControlador(){
+        banco = new Banco();
+    }
+
+    @FXML
+    public void iniciarSesion(ActionEvent event) {
+        String id = txtId.getText();
+        String password = txtPassword.getText();
+
+        Usuario cliente = banco.iniciarSesion(id, password);
+
+        if (usuario != null) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/banco/vista/panelCliente.fxml"));
+                Parent root = loader.load();
+
+                PanelClienteControlador controlador = loader.getController();
+                controlador.setUsuario(usuario);
+
+                Stage stage = (Stage) txtUsuario.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.show();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            // Muestra una alerta de error
+            Alert alerta = new Alert(Alert.AlertType.ERROR);
+            alerta.setTitle("Error de inicio de sesión");
+            alerta.setHeaderText(null);
+            alerta.setContentText("Usuario o contraseña incorrectos");
+            alerta.showAndWait();
+        }
+    }
+
+
+
 
 
 }
--- src/main/java/co/edu/uniquindio/banco/controlador/PanelClienteControlador.java
+++ src/main/java/co/edu/uniquindio/banco/controlador/PanelClienteControlador.java
@@ -1,9 +1,18 @@
 package co.edu.uniquindio.banco.controlador;
 
+import co.edu.uniquindio.banco.modelo.entidades.Usuario;
+
 /**
  * Clase que se encarga de gestionar las acciones de la interfaz gráfica del panel del cliente.
  * @author caflorezvi
  */
 public class PanelClienteControlador {
+
+    private Usuario usuario;
+
+    public void setCliente(Usuario usaurio) {
+        this.usuario = usuario;
+    }
+
 
 }
