8b2640adbcd1927e3ce03af86a06646c262ddff7
michaelj305 <maicol.alvarezuwu@gmail.com>
2025-04-08 20:35:52 -0500

    se crearon las interfaces de actualizar datos y recargar billetera

---
+++ src/main/java/co/edu/uniquindio/banco/controlador/ActualizarDatosControlador.java
@@ -0,0 +1,31 @@
+package co.edu.uniquindio.banco.controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ActualizarDatosControlador {
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtDireccion;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private TextField txtIdentificacion;
+
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private PasswordField txtPassword;
+
+}
