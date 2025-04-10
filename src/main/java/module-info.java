baef3a4903ff5efd13b3b8470fa448edd0a443be
serrrrrgio <sergio.posadag@uqvirtual.edu.co>
2025-04-09 21:07:30 -0500

    se organiza el module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,15 +3,12 @@ module co.edu.uniquindio.banco {
     requires javafx.fxml;
     requires static lombok;
 
-    opens co.edu.uniquindio.banco to javafx.fxml;
-
     exports co.edu.uniquindio.banco;
     exports co.edu.uniquindio.banco.modelo.entidades;
     exports co.edu.uniquindio.banco.modelo.vo;
-
     exports co.edu.uniquindio.banco.controlador;
-    opens co.edu.uniquindio.banco.controlador to javafx.fxml;
-    exports co.edu.uniquindio.banco.modelo;
     exports co.edu.uniquindio.banco.modelo.Singleton;
-    opens co.edu.uniquindio.banco.modelo to javafx.fxml;
+
+    opens co.edu.uniquindio.banco.controlador to javafx.fxml;
+
 }
